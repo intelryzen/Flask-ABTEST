dca09aef1640f4a323332ec09f46be16f30c88ef
우승식 <softabledev@gmail.com>
2023-07-01 00:12:58 +0900

    구독취소기능(로그아웃), 쿠키 지속시간 늘림

--- blog_view/blog.py
+++ blog_view/blog.py
@@ -1,5 +1,6 @@
 from flask import Flask, Blueprint, request, render_template, make_response, jsonify, redirect, url_for
-from flask_login import login_user, current_user
+from flask_login import login_user, current_user, logout_user
+from datetime import timedelta
 from blog_control.user_mgmt import User
 
 blog_abtest = Blueprint('blog_abtest', __name__)
@@ -15,13 +16,19 @@ def set_email():
         # x-www-form 인 경우 request.form 을 써야함 주로 html 에서 post 로 호출할떼 form인 경우가 많음
         print('set_email', request.form['user_email'])
         user = User.create(request.form['user_email'], 'A')
-        login_user(user) # 사용자 request 의 header (사용자의 ip)와 user 객체의 id, 서버의 secret_key 를 기반으로 session 을 만듦. session 은 flask_login 만의 기능이 아닌 flask 자체의 기능임.
+        login_user(user, remember=True, duration=timedelta(days=31)) # 사용자 request 의 header (사용자의 ip)와 user 객체의 id, 서버의 secret_key 를 기반으로 session 을 만듦. session 은 flask_login 만의 기능이 아닌 flask 자체의 기능임.
         
     # return make_response(jsonify(success = True), 200)
     return redirect(url_for('blog_abtest.test_blog')) 
     # redirect 는 라우팅 경로를 다르게 해서 리턴함 (ex. get 방식인 경우 응답할때 설정한 라우팅 경로로 초기화.) url_for은 redirect('/blog/test_blog')와 동일한 역할
   
+    # redirect 는 라우팅 경로를 다르게 해서 리턴함 (ex. get 방식인 경우 응답할때 설정한 라우팅 경로로 초기화.) url_for은 redirect('/blog/test_blog')와 동일한 역할
 
+@blog_abtest.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('blog_abtest.test_blog')) 
+    
 @blog_abtest.route('/test_blog')
 def test_blog():
     # Flask 에서는 세션을 user_id 별로 관리
