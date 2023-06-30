24f3312f8e52fc74d77b113e842ae32e69b500f0
우승식 <softabledev@gmail.com>
2023-06-30 18:21:39 +0900

    blog_abtest 추가

---
+++ blog_abtest.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify, request, render_template, make_response
+from flask_login import LoginManager, current_user, login_required, login_user, logout_user
+from flask_cors import CORS
+import os
+
+# https 만을 지원하는 기능을 http 에서 테스트할 때 필요한 설정
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = '1'
+
+app = Flask(__name__, static_url_path='/static') # html 파일 내 필요한 소스는 static에서 가져옴.
+CORS(app) # 여기서는 사실 필요없음.
+app.secret_key = 'server Key'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.session_protection = "strong"
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
+
+@login_manager.unauthorized_handler # 로그인하지 않은 사용자가 비인가(로그인 필요)된 api를 호출하면
+def unauthorized():
+    return make_response(jsonify(success = False), 401)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='8080', debug=True)
